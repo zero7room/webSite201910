8ec98f9e7277291e26ad62f5c76351c1391c70d6
wangzqf <wangzqf@yonyou.com>
2019-09-20 17:20:37 +0800

    new2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function handle_request(req, res) {
 
     // 客户端对服务器的请求，说白了就是对相关文件内容的请求。
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(get_file_content(__dirname + '\\' + 'html' + '\\' + 'index.html'));
+    res.end(get_file_content(__dirname + '/' + 'html' + '/' + 'index.html'));
 }
 
 function get_file_content(filepath) {
