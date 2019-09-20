c729afc2454d4c78c04380cf80bd9e59a8a08cb0
wangzqf <wangzqf@yonyou.com>
2019-09-20 17:16:10 +0800

    new

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+
+function handle_request(req, res) {
+
+    // 客户端对服务器的请求，说白了就是对相关文件内容的请求。
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(get_file_content(__dirname + '\\' + 'html' + '\\' + 'index.html'));
+}
+
+function get_file_content(filepath) {
+    return fs.readFileSync(filepath);
+}
+
+var server = http.createServer(handle_request);
+server.listen(8080);
